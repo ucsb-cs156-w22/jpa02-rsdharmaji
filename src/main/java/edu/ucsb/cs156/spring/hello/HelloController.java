f19a12e4d9698edb658b5e1db67c38ae4ba8107d
pkishi <13582030+pkishi@users.noreply.github.com>
2022-01-07 12:42:32 -0800

    add '!' to application

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from rdharmaji";
+        return "Greetings from rdharmaji!";
     }
     
 }
